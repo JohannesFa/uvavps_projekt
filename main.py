a015ed197aeafe20e6718fcd2dd582de04b10a79
Johannes <jo2144fa-s@student.lu.se>
2026-04-08 15:34:36 +0200

    :)

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
+import ollama
 from ollama import chat, ChatResponse
 import pandas as pd
 
 models_list = ['falcon3:1b', 'qwen3.5:0.8b']
 
+def check_if_models_exist(model_list: list) :
+    ollama_list = ollama.list()
+    installed_models = []
+    for x in ollama_list['models'] :
+        installed_models.append(x['model'])
+
+
+    found_not_installed_model = False
+    for model in model_list:
+        if model not in installed_models:
+            print(f"Model {model} not installed or misspelled")
+            found_not_installed_model = True
+    if found_not_installed_model:
+        print("Exiting beacuse all models not installed")
+        exit()
+            
+
 def askQuestion(question:str, model:str) -> str:
-    response: ChatResponse = chat(model='falcon3:1b', think=False, messages=[
+    response: ChatResponse = chat(model=model, think=False, messages=[
     {
         'role': 'user',
         'content': question,
@@ -13,8 +31,14 @@ def askQuestion(question:str, model:str) -> str:
     return response.message.content
 
 
-questionsdf: pd.DataFrame = pd.read_csv("questions.csv",delimiter=";")
 
-for question in questionsdf["Question"]:
-    print(question)
-    #print(askQuestion("What color is the sky?", models_list[0]))
+if __name__ == "__main__":
+
+    check_if_models_exist(models_list)
+
+    questionsdf: pd.DataFrame = pd.read_csv("questions.csv",delimiter=";")
+
+    for question in questionsdf["Question"]:
+        print(f"Question is {question}, expected answer is {""}")
+        
+        #print(askQuestion("What color is the sky?", models_list[0]))
