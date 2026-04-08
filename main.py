844b5897a427c3bb0a368f7f7a5013583d285ead
hanroid <hannes.akerberg@gmail.com>
2026-04-08 15:25:25 +0200

    fixade lite importer

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from ollama import chat, ChatResponse
+from ollama import chat, ChatResponse, list
 import pandas as pd
 
 models_list = ['falcon3:1b', 'qwen3.5:0.8b']
+questionsdf: pd.DataFrame = pd.read_csv("questions.csv",delimiter=";")
 
 def run_model(model: str) :
-
-    ollama_list = ollama.list()
+    ollama_list = list()
     installed_models_list = []
     for x in ollama_list['models'] :
         installed_models_list.append(x['model'])
@@ -16,6 +16,7 @@ def run_model(model: str) :
     file_out.write(f"{model},")
     file_out.close()
 
+
 def askQuestion(question:str, model:str) -> str:
     response: ChatResponse = chat(model=model, think=False, messages=[
     {
@@ -24,9 +25,6 @@ def askQuestion(question:str, model:str) -> str:
     },
     ])
     return response.message.content
-
-
-questionsdf: pd.DataFrame = pd.read_csv("questions.csv",delimiter=";")
 
 for question in questionsdf["Question"]:
     print(question)
