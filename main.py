9da16a7a9eb01d7fd51b96e1565118596f646e9c
hanroid <hannes.akerberg@gmail.com>
2026-04-08 15:22:13 +0200

    Moved run_model to main and deleted ai_script

--- main.py
+++ main.py
@@ -3,8 +3,21 @@ import pandas as pd
 
 models_list = ['falcon3:1b', 'qwen3.5:0.8b']
 
+def run_model(model: str) :
+
+    ollama_list = ollama.list()
+    installed_models_list = []
+    for x in ollama_list['models'] :
+        installed_models_list.append(x['model'])
+    if model not in installed_models_list:
+        print(f"Model {model} not installed or misspelled")
+        return
+    file_out = open("results.csv", "w")
+    file_out.write(f"{model},")
+    file_out.close()
+
 def askQuestion(question:str, model:str) -> str:
-    response: ChatResponse = chat(model='falcon3:1b', think=False, messages=[
+    response: ChatResponse = chat(model=model, think=False, messages=[
     {
         'role': 'user',
         'content': question,
