91bb42f6c6048090e3c59ba004cd4d2ce9896c21
hanroid <hannes.akerberg@gmail.com>
2026-04-14 23:21:14 +0200

    Implemented a function to calculate the percentage of correct answers for a given model, ignores domains

--- statistical_analysis.py
+++ statistical_analysis.py
@@ -3,6 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.stats as stats
 
+def total_percentage_correct(filename: str):
+    df = pd.read_csv(filename, sep=";")
+    df['Model_correct'] = df['Model_correct'].astype(int)
+    return df['Model_correct'].mean() * 100
 
-def percentage_correct(filename: str):
-    df = pd.read_csv(filename)
+
+
